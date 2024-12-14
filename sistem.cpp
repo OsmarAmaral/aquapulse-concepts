fb52d2cb7ae58b71965647d841e47ca015e51144
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-12-13 22:04:12 -0400

    Create sistem.cpp

---
+++ sistem.cpp
@@ -0,0 +1,33 @@
+// AquaPulse - Irrigação Automatica com Arduino
+
+const int soilMoistureSensor = AO; /* Sensor de umidade*/
+const int button = 2; /* Botão */
+const int waterPump = 3; /* Bomba de água */
+
+int minimunSoilMoisture = 307; /* Umidade minima = 30% */
+int delayTime = 6000; /* 6 segundos */
+
+void setup () {
+    pinMode(soilMoistureSensor, INPUT);
+    pinMode(button, INPUT);
+    pinMode(waterPump, OUTPUT);
+
+    digitalWrite(waterPump, LOW);
+    Serial.begin(9600);
+}
+
+void loop () {
+    int buttonState = digitalRead(button); /* leitura do botão */
+    Serial.printl("Button: "+buttonState);
+    if(buttonState == HIGH) {
+        /* se estiver ligado */
+        int currentSoilMoisture = analogRead(soilMoistureSensor); /* leitura da umidade*/
+        Serial.printl("Umidade: "+currentSoilMoisture);
+        if(currentSoilMoisture < minimunSoilMoisture) {
+            /* se ela for menor que a minima */
+            digitalWrite(waterPump, HIGH); /* ative a bomba */
+            delay(delayTime); /* espera um tempo */
+            digitalWrite(waterPump, LOW); /* desligue */
+        }
+    }
+}
